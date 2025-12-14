8254238046aa390331ef1dc729bc62d52d499a7a
Abhilash S <abhilashsubhash72@gmail.com>
2025-12-14 17:03:12 +0530

    Updated contact me section with <p> tag for mobile

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,7 +22,7 @@ const Contact = () => {
                             <span className="method-value">abhilashsubhash72@gmail.com</span>
                         </a>
 
-                        <a href="" className="method-card">
+                        <p className="method-card">
                             <div className="method-icon">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" /></svg>
                             </div>
@@ -30,7 +30,7 @@ const Contact = () => {
                             <span className="method-value">9746534378</span>
                             <h6>OR</h6>
                             <span className="method-value">9846980759</span>
-                        </a>
+                        </p>
 
                         <a href="https://www.linkedin.com/in/abhilash-s-617130389/" className="method-card">
                             <div className="method-icon">
